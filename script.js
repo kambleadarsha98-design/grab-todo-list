41c5f47cbc7f3812f5f5eb37e3710d92e2e4f191
kambleadarsha98@gmail.com <kambleadarsha98@gmail.com>
2025-12-05 00:41:32 +0530

    new  patch

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const todo = document.querySelector("#Todo");
-const progress = document.querySelector("#progress");
+const  working = document.querySelector("#progress");
 const done = document.querySelector("#Done");
 
-const tasks = document.querySelectorAll(".task1");
+const tasks = document.querySelectorAll(".task1"); 
+
+
+
+let drag = null ; 
 
 tasks.forEach(task => {
-    task.addEventListener("dragstart", (e) => {
-        console.log("dragging", e.target);
+    task.addEventListener("drag", (e) => {
+    drag = task ;
     });
 });
 
+function  addDragEventsOnColumn(column){
+    column.addEventListener("dragenter", (e) =>{
+         e.preventDefault();
+        column.classList.add("hover-over");
+    })
 
+ column.addEventListener("dragleave", (e) =>{
+         e.preventDefault();
+        column.classList.remove("hover-over");
+    }) 
 
+     column.addEventListener("dragover",(e)=>{
+        e.preventDefault()
+     })
+
+column.addEventListener("drop", (e) => {
+    e.preventDefault(); 
+      console.log("dropped", drag, column);
+      
+        column.appendChild(drag);
+
+      column.classList.remove("hover-over")
+    
+    
+});
+
+}
+
+addDragEventsOnColumn(done);
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(working);
