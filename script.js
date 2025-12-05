8ded547fbc5c25837f5d4853b55ca008cb8e27a8
kambleadarsha98@gmail.com <kambleadarsha98@gmail.com>
2025-12-06 00:12:09 +0530

    new  patch

--- script.js
+++ script.js
@@ -45,3 +45,44 @@ column.addEventListener("drop", (e) => {
 addDragEventsOnColumn(done);
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(working);
+
+
+
+
+const openBtn = document.getElementById('open-task-modal-btn');
+const closeBtn = document.getElementById('close-task-modal-btn');
+const modal = document.getElementById('task-modal-overlay');
+
+// 1. When 'Add New Task' is clicked, show the modal
+openBtn.addEventListener('click', () => {
+    modal.classList.add('show');
+});
+
+// 2. When 'Submit Task' (or close button) is clicked, hide the modal
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+});
+
+// Optional: Hide modal if user clicks on the blurred background itself
+modal.addEventListener('click', (event) => {
+    // Check if the click target is the modal overlay, not the form inside it
+    if (event.target === modal) {
+        modal.classList.remove('show');
+    }
+});
+
+    // 1. Get the HTML element objects
+
+    let  title  =   document.getElementById("heading") 
+
+    let  headingTitle =   document.getElementById("task-title")
+ 
+
+    headingTitle.textContent =   title.value ; 
+
+
+    console.log(headingTitle);
+
+
+
+  
